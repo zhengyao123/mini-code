5f682edc98f924ad7508d3c1e1aba127ca25e498
zhengyao <1132201989@qq.com>
2020-07-02 17:42:11 +0800

    基于redis的接口限流

---
+++ mini-code-redis/src/test/java/com/zy/minicoderedis/MiniCodeRedisApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zy.minicoderedis;
+
+import com.zy.minicoderedis.common.RedisService;
+import com.zy.minicoderedis.user.TestKey;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+class MiniCodeRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
