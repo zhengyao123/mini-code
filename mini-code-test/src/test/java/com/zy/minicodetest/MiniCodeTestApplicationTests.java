7033b8a43fd3e730a81ba94197047ca946a443f1
zhengyao <1132201989@qq.com>
2020-07-07 09:54:28 +0800

    spi扩展redis的lock-start

---
+++ mini-code-test/src/test/java/com/zy/minicodetest/MiniCodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.minicodetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCodeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
